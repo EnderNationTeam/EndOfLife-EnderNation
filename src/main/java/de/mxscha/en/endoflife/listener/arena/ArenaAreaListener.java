e1194c98848f90a6df8525ff05970d9e4ef7aa25
keksgauner <paul@rakutt.eu>
2023-01-11 16:56:38 +0100

    Added: ArenaArea Remove Auto Water

--- src/main/java/de/mxscha/en/endoflife/listener/arena/ArenaAreaListener.java
+++ src/main/java/de/mxscha/en/endoflife/listener/arena/ArenaAreaListener.java
@@ -4,16 +4,16 @@ import de.mxscha.en.endoflife.EndoflifeCore;
 import de.mxscha.en.endoflife.commands.world.BuildCommand;
 import de.mxscha.en.endoflife.utils.manager.location.ConfigLocationUtil;
 import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.*;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class ArenaAreaListener implements Listener {
@@ -61,13 +61,17 @@ public class ArenaAreaListener implements Listener {
         Location arenaPvp1 = new ConfigLocationUtil("ArenaPvp1").loadLocation();
         Location arenaPvp2 = new ConfigLocationUtil("ArenaPvp2").loadLocation();
 
+        ItemStack itemStack = event.getItem();
 
-        Material material = event.getItem().getType();
+        if(itemStack == null) {
+            return;
+        }
+
+        Material material = itemStack.getType();
         if((event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) && !(
                 material.equals(Material.BOW)
                     || material.equals(Material.BOWL)
                     || material.equals(Material.CROSSBOW)
-                    || material.equals(Material.BOW)
                     || material.equals(Material.EGG)
                     || material.equals(Material.SNOWBALL)
                     || material.equals(Material.ENDER_PEARL)
@@ -78,56 +82,12 @@ public class ArenaAreaListener implements Listener {
         }
 
         if (EndoflifeCore.getInstance().getRegionManager().isIn(player.getLocation(), arenaPvp1, arenaPvp2)) {
-            if(!material.equals(Material.EGG) || material.equals(Material.WATER_BUCKET) || material.equals(Material.LAVA_BUCKET) || material.equals(Material.BUCKET)) {
+            if((!material.equals(Material.EGG)) || material.equals(Material.WATER_BUCKET) || material.equals(Material.FLINT_AND_STEEL) || material.equals(Material.LAVA_BUCKET) || material.equals(Material.BUCKET)) {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        if(event.getClickedBlock() != null) {
-                            cancel();
-                        }
-                        Location location = event.getClickedBlock().getLocation().add(0, 1, 0);
-                        Block block = location.getBlock();
-                        if(block.getType().equals(Material.WATER)) {
-                            block.setType(Material.AIR);
-
-                            Location locationPlusX = location.add(1,0,0);
-                            Location locationMinusX = location.add(-1,0,0);
-                            Location locationPlusZ = location.add(0,0,1);
-                            Location locationMinusZ = location.add(0,0,-1);
-                            if(locationPlusX.getBlock().getType().equals(Material.WATER)) {
-                                locationPlusX.getBlock().setType(Material.AIR);
-                            }
-                            if(locationMinusX.getBlock().getType().equals(Material.WATER)) {
-                                locationMinusX.getBlock().setType(Material.AIR);
-                            }
-                            if (locationPlusZ.getBlock().getType().equals(Material.WATER)) {
-                                locationPlusZ.getBlock().setType(Material.AIR);
-                            }
-                            if (locationMinusZ.getBlock().getType().equals(Material.WATER)) {
-                                locationMinusZ.getBlock().setType(Material.AIR);
-                            }
-                        }
-
-                        if(block.getType().equals(Material.LAVA)) {
-                            block.setType(Material.AIR);
-
-                            Location locationPlusX = location.add(1,0,0);
-                            Location locationMinusX = location.add(-1,0,0);
-                            Location locationPlusZ = location.add(0,0,1);
-                            Location locationMinusZ = location.add(0,0,-1);
-                            if(locationPlusX.getBlock().getType().equals(Material.LAVA)) {
-                                locationPlusX.getBlock().setType(Material.AIR);
-                            }
-                            if(locationMinusX.getBlock().getType().equals(Material.LAVA)) {
-                                locationMinusX.getBlock().setType(Material.AIR);
-                            }
-                            if (locationPlusZ.getBlock().getType().equals(Material.LAVA)) {
-                                locationPlusZ.getBlock().setType(Material.AIR);
-                            }
-                            if (locationMinusZ.getBlock().getType().equals(Material.LAVA)) {
-                                locationMinusZ.getBlock().setType(Material.AIR);
-                            }
-                        }
+                        removeLiquid(Material.WATER, event.getClickedBlock());
+                        removeLiquid(Material.LAVA, event.getClickedBlock());
                     }
                 }.runTaskLater(EndoflifeCore.getInstance(), 20*5);
                 return;
@@ -148,6 +108,46 @@ public class ArenaAreaListener implements Listener {
             if (!BuildCommand.getBuild().contains(player)) {
                 event.setCancelled(true);
             }
+        }
+    }
+
+    @EventHandler
+    public void noLiquidFlow(BlockPhysicsEvent event) {
+        Location arenaSpawn = new ConfigLocationUtil("ArenaSpawn").loadLocation();
+
+        if (event.getBlock().getLocation().getWorld().getName().equals(arenaSpawn.getWorld().getName())) {
+            // This is thrown when a liquid block is about to flow
+            Block block = event.getBlock();
+            if(block.getType().equals(Material.WATER) || block.getType().equals(Material.LAVA)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void noLiquidFlow(BlockFromToEvent event) {
+        Location arenaSpawn = new ConfigLocationUtil("ArenaSpawn").loadLocation();
+
+        if (event.getBlock().getLocation().getWorld().getName().equals(arenaSpawn.getWorld().getName())) {
+            // This is thrown anytime a liquid flows
+            Block block = event.getBlock();
+            if(block.getType().equals(Material.WATER) || block.getType().equals(Material.LAVA)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    private void removeLiquid(Material material, Block clickedBlock) {
+
+        if(clickedBlock == null) {
+            return;
+        }
+
+        Location location = clickedBlock.getLocation().add(0, 1, 0);
+        Block block = location.getBlock();
+
+        if(block.getType().equals(material)) {
+            block.setType(Material.AIR);
         }
     }
 }
